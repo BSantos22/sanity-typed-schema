8495a417f023ce3cead888dffb40bf8627887eb7
Bruno Santos <bruno.santos1996@gmail.com>
2023-07-14 08:36:41 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ts-script-starter
+
+Basic starter project to create Typescript scripts.
+
+## Usage
+
+Clone the repo and run:
+
+```
+yarn
+yarn test
+```
---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { main } from 'src';
+import {describe, expect, it} from 'vitest';
+
+describe('ts-script-starter', () => {
+	it('main', async () => {
+		expect(main()).toEqual(1);
+	});
+});
